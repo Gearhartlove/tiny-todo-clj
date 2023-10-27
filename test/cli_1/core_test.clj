5d30c6621163dd2f9338536a0662868b9d11f747
Gearhartlove <kristoffgearhartlove@gmail.com>
2023-10-26 23:17:37 -0600

    tests passing, yay!

--- test/cli_1/core_test.clj
+++ test/cli_1/core_test.clj
@@ -2,6 +2,24 @@
   (:require [clojure.test :refer :all]
             [cli-1.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest clear-test
+  (testing "Attempting to clear a file")
+  (is (= true (clear "/Users/gearhart/projects/clojure/cli/resources/debug_todo.txt" ))))
+
+(deftest prepare-data-test
+  (test "Preparing the data to look like :todo[]/n:complete[] in csv for consistent parsing")
+  (let [data {:todo ["Go to the store" "Rock climb" "Take regina to the harry potter candy store"], :complete '("program for work" "try harder")}
+        writeToFile (str ":todo " (get data :todo) "\n" ":complete " (get data :complete))]
+    (is (=
+          writeToFile
+          ":todo [\"Go to the store\" \"Rock climb\" \"Take regina to the harry potter candy store\"]\n:complete (\"program for work\" \"try harder\")"))))
+
+(deftest save-test
+  (test "Attempting to save data in the correct format")
+  (let [test-file "/Users/gearhart/projects/clojure/cli/resources/debug_todo.txt"
+        data {:todo ["Go to the store" "Rock climb" "Take regina to the harry potter candy store"], :complete '("program for work" "try harder")} ]
+    (clear test-file)
+    (save data test-file)
+    (is (=
+          ":todo [\"Go to the store\" \"Rock climb\" \"Take regina to the harry potter candy store\"]\n:complete (\"program for work\" \"try harder\")"
+          (slurp test-file)))))
