770fb245d335ed4aed804f9412f935eb3c5191b3
Gearhartlove <kristoffgearhartlove@gmail.com>
2023-10-27 14:24:03 -0600

    new tests passing! yay!

--- test/cli_1/core_test.clj
+++ test/cli_1/core_test.clj
@@ -1,25 +1,38 @@
 (ns cli-1.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [cli-1.core :refer :all]))
+
+; used to reset files for when resetting tests
+(defn clear [file] (io/delete-file file true))
+(defn test-file [] "/Users/gearhart/projects/clojure/cli/resources/todo-test.toml")
+(defn test-data [] {:todo-list {:todo ["foo" "fuz"] :completed ["bar"]}})
 
 (deftest clear-test
   (testing "Attempting to clear a file")
-  (is (= true (clear "/Users/gearhart/projects/clojure/cli/resources/debug_todo.txt" ))))
-
-(deftest prepare-data-test
-  (test "Preparing the data to look like :todo[]/n:complete[] in csv for consistent parsing")
-  (let [data {:todo ["Go to the store" "Rock climb" "Take regina to the harry potter candy store"], :complete '("program for work" "try harder")}
-        writeToFile (str ":todo " (get data :todo) "\n" ":complete " (get data :complete))]
-    (is (=
-          writeToFile
-          ":todo [\"Go to the store\" \"Rock climb\" \"Take regina to the harry potter candy store\"]\n:complete (\"program for work\" \"try harder\")"))))
+  (is (= true (clear "/Users/gearhart/projects/clojure/cli/resources/debug_todo.toml" ))))
 
 (deftest save-test
   (test "Attempting to save data in the correct format")
-  (let [test-file "/Users/gearhart/projects/clojure/cli/resources/debug_todo.txt"
-        data {:todo ["Go to the store" "Rock climb" "Take regina to the harry potter candy store"], :complete '("program for work" "try harder")} ]
-    (clear test-file)
-    (save data test-file)
-    (is (=
-          ":todo [\"Go to the store\" \"Rock climb\" \"Take regina to the harry potter candy store\"]\n:complete (\"program for work\" \"try harder\")"
-          (slurp test-file)))))
+  (clear (test-file))
+  (save (test-data) (test-file))
+  (is (=
+        "\n[todo-list]\ntodo = [\"foo\", \"fuz\"]\ncompleted = [\"bar\"]\n"
+        (slurp (test-file)))))
+
+(deftest parse-test
+  (test "Able to read from file and turn into map of toml values")
+  (clear (test-file))
+  (save (test-data) (test-file))
+  (is (=
+        {:todo-list {:todo ["foo" "fuz"] :completed ["bar"]}}
+        (parse-toml-file (test-file)))))
+
+(deftest get-chain
+  (clear (test-file))
+  (save (test-data) (test-file))
+  (is (=
+        ["foo" "fuz"]
+        (get-in (parse-toml-file (test-file)) [:todo-list :todo]))))
+
+
